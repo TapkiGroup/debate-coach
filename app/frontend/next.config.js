4624b6781e8aa825fb1c198367df300910e30db2
Inna Vays <129948497+InnaVays@users.noreply.github.com>
2025-08-23 13:01:35 +0200

    Update backend URL in next.config.js

--- app/frontend/next.config.js
+++ app/frontend/next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL || "http://49.13.2.245:8001/api"
+    NEXT_PUBLIC_BACKEND_URL:
+      process.env.NEXT_PUBLIC_BACKEND_URL || "https://debate-mate.onrender.com"
   }
 };
