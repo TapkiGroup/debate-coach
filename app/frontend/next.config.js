0e5f67fd06d1f97817b1b4ff8b56c262dfaaeb85
Inna Vays <129948497+InnaVays@users.noreply.github.com>
2025-08-23 13:38:59 +0200

    Enable unoptimized images in Next.js config

--- app/frontend/next.config.js
+++ app/frontend/next.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   reactStrictMode: true,
+  images: { unoptimized: true },
+  output: 'export',                 
   env: {
     NEXT_PUBLIC_BACKEND_URL:
       process.env.NEXT_PUBLIC_BACKEND_URL || "https://debate-mate.onrender.com/api"
