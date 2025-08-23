763766b80f5ec0996925d375157a09650ae68b3c
Inna Vays <129948497+InnaVays@users.noreply.github.com>
2025-08-23 14:10:19 +0200

    Change backend URL to use relative path

--- app/frontend/next.config.js
+++ app/frontend/next.config.js
@@ -1,9 +1,8 @@
 module.exports = {
   reactStrictMode: true,
   images: { unoptimized: true },
-  output: 'export',                 
   env: {
     NEXT_PUBLIC_BACKEND_URL:
-      process.env.NEXT_PUBLIC_BACKEND_URL || "https://debate-mate.onrender.com/api"
+      process.env.NEXT_PUBLIC_BACKEND_URL || "/api"
   }
 };
